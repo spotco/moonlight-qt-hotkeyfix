4b14f62020af5df5dbc3f54df5f88ad3a1d082b1
spotco <mootothemax@gmail.com>
2026-09-19 18:21:31 +0000

    fix: include nvaddress.h; avoid std::move in host UDP task

--- app/gui/computermodel.cpp
+++ app/gui/computermodel.cpp
@@ -1,4 +1,5 @@
 #include "computermodel.h"
+#include "backend/nvaddress.h"
 
 #include <QThreadPool>
 #include <QUdpSocket>
@@ -254,7 +255,7 @@ class DeferredHostUdpTestTask : public QObject, public QRunnable
     Q_OBJECT
 public:
     DeferredHostUdpTestTask(QString host, uint16_t httpPort)
-        : m_Host(std::move(host)), m_HttpPort(httpPort) {}
+        : m_Host(host), m_HttpPort(httpPort) {}
 
     void run()
     {
